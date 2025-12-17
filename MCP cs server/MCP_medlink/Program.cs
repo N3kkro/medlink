673994486f16314de91788ed2bcce93b8e76b1be
unknown <sayatkenesov159@gmail.com>
2025-12-17 14:56:50 +0600

    commit message

---
+++ MCP cs server/MCP_medlink/Program.cs
@@ -0,0 +1,23 @@
+﻿using MCP_medlink;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+builder.Services.AddSingleton<ResponseToComplaint>();
+builder.Services.AddSingleton<AppointmentValidation>();
+builder.Services.AddSingleton<DoctorAvailable>();
+await builder.Build().RunAsync();
+
